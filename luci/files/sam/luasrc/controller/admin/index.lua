136020d559c197ec0ad2e0c91ead725af6a140d0
Michael Liang <mliang@codeaurora.org>
2012-07-19 16:28:48 -0700

    [luci-mod-sam] Basic scan & join support
    The following changes are applied:
    1. Remove scripts which are not needed by luci-mod-sam;
    2. Add jquery and jquery UI library;
    3. Rewrite wifi_join page to support basic scan & join action described
    by functional spec;
    
    Signed-off-by: Michael Liang <mliang@codeaurora.org>
    
    Change-Id: I1925fff9856bd9c1f4b97ba866bb321da6489fa4
    Signed-off-by: Michael Liang <mliang@codeaurora.org>

--- luci/files/sam/luasrc/controller/admin/index.lua
+++ luci/files/sam/luasrc/controller/admin/index.lua
@@ -25,17 +25,18 @@ function index()
 	page.target  = firstchild()
 	page.title   = _("Administration")
 	page.order   = 10
-	page.sysauth = "root"
-	page.sysauth_authenticator = "htmlauth"
+	--page.sysauth = "root"
+	--page.sysauth_authenticator = "htmlauth"
 	page.ucidata = true
 	page.index = true
 
 	-- Empty services menu to be populated by addons
 	entry({"admin", "services"}, firstchild(), _("Services"), 40).index = true
 
-	entry({"admin", "logout"}, call("action_logout"), _("Logout"), 90)
+	--entry({"admin", "logout"}, call("action_logout"), _("Logout"), 90)
 end
 
+--[[
 function action_logout()
 	local dsp = require "luci.dispatcher"
 	local sauth = require "luci.sauth"
@@ -47,3 +48,4 @@ function action_logout()
 	luci.http.header("Set-Cookie", "sysauth=; path=" .. dsp.build_url())
 	luci.http.redirect(luci.dispatcher.build_url())
 end
+--]]
