bfcf691e4c52a8e218ed74db70a5d0c255511baf
Michael Liang <mliang@codeaurora.org>
2012-09-13 19:12:31 +0800

    [luci-mod-sam] Change to async mode for wifi connection monitoring
    1. Use event action script feature of wpa_cli to handle
    connect/disconnect event from wpa_supplicant;
    2. Start a script to countdown on disconnect event, once timer timeout
    reached, restore to default wireless configuration;
    3. Kill any countdown process on connect event;
    
    Change-Id: I1ad1e3ece7bc5e19f5018bb96e4b1437fffc6426
    Signed-off-by: Michael Liang <mliang@codeaurora.org>

---
+++ luci/files/modules/sam/root/usr/sbin/sam_action.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ -z "$1" -o -z "$2" ]; then
+    exit 1
+fi
+
+WPA_IFACE="$1"
+WPA_EVENT="$2"
+echo "EVENT $WPA_EVENT on $WPA_IFACE" >/dev/console
+case "$WPA_EVENT" in
+    "CONNECTED")
+        killall -q sam_countdown
+        ;;
+    "DISCONNECTED")
+        sam_countdown &
+        ;;
+    *)
+        echo "Unknown action: $WPA_ACTION" >/dev/console
+        exit 1
+        ;;
+esac
+
+exit 0
